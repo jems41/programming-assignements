96488a1ac4699238c5db1c33f0ced26c4ea760ec
James Ferdinand Combista <james.combista@dawsoncollege.qc.ca>
2024-10-22 11:13:18 -0400

    we have lost out minds

--- uno.py
+++ uno.py
@@ -1,4 +1,4 @@
-# AI and Merging implemented with David Hack
+# code by David Hack and James Combista
 # implemented +2 and Skip Turn
 
 import random
@@ -6,29 +6,31 @@ import random
 
 def start_game():
     colours = ("Red", "Yellow", "Green", "Blue")
-    ranks = list(range(1, 11)) + ["+2"] + ["Skip Turn"] # Add "Skip Turn" to possible cards
+    ranks = list(range(1, 11)) + ["+2"] + ["Skip Turn"]  # Add "+2" and Skip Turn as special ranks
     deck = [(rank, colour) for rank in ranks for colour in colours]
     random.shuffle(deck)
 
-    p1 = [deck.pop(0) for _ in range(7)]
-    p2 = [deck.pop(0) for _ in range(7)]
+    p1 = [deck.pop(0) for _ in range(1)]
+    p2 = [deck.pop(0) for _ in range(1)]
 
     central_card = deck.pop(0)
     main_loop(p1, p2, deck, central_card, 0)
 
 def main_loop(p1, p2, deck, central_card, whose_turn):
-    while len(p1)>0 and len(p2)>0:
+    count = 0
+    while len(p1) > 0 and len(p2) > 0:
+
         print(f"\nPlayer {whose_turn + 1}'s turn, here is your hand {p1}")
         print(f"Central card is: {central_card}")
 
 
         ans = int(input("You have a choice. You can (0) draw or (1) play: "))
-        if ans != 1 or ans != 0:
+        if ans != 1 and ans != 0:
             for _ in range(100):
                 ans = int(input("Bad Choice. You can (0) draw or (1) play: "))
                 if ans == 1 or ans == 0:
                     break
-
+        skip_turn = True
         if ans == 1:
             valid_play_found = "No :("
 
@@ -37,6 +39,7 @@ def main_loop(p1, p2, deck, central_card, whose_turn):
                 if 0 <= player_choice < len(p1):
                     valid = valid_play(central_card, p1[player_choice])
                     if valid:
+                        player_card = p1[player_choice]
                         central_card = p1.pop(player_choice)
                         valid_play_found = "Yup"
                     else:
@@ -44,25 +47,41 @@ def main_loop(p1, p2, deck, central_card, whose_turn):
                 else:
                     print("Invalid card selection. Please try again.")
 
-            plus2_flag = True
-            if central_card[0] == "+2" and plus2_flag==True:
+# +2 card code
+
+            if ((player_card)[0]) == "+2":
                 print("The opponent must draw 2 cards!")
                 p2.append(deck.pop(0))
                 p2.append(deck.pop(0))
-                plus2_flag = False
-        
-            switch_card = True
-            if central_card[0] == "Skip Turn" and switch_card == True:
-                whose_turn = (whose_turn + 1)
 
+# Skip turn card code ->
+        
+            if central_card[0] == "Skip Turn" and skip_turn == True:
+                print("The opponent turn has been skipped!")
+                whose_turn = (whose_turn + 1)
+                skip_turn = False
+                count += 1
 
         elif ans == 0:
             draw_card = deck.pop(0)
             p1.append(draw_card)
 
-        p1, p2 = p2, p1
+        if skip_turn:
+            p1, p2 = p2, p1
+
+        count += 1
+
         whose_turn = (whose_turn + 1) % 2
 
+# Determining winner code\
+played_card = p1.pop(player_choice)
+                    central_card = played_card
+                    valid_play_found = True
+    if count % 2 == 0:
+        print(f"Player 2 wins")
+    else:
+        print("Player 1 wins!!!1")
+        
 def valid_play(card1, card2):
     return card1[0] == card2[0] or card1[1] == card2[1]
 
